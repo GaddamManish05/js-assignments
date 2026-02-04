f1d6d48b582a6d17b04cb33fb9607cf37ef14023
GaddamManish05 <gaddam.mani1305@gmail.com>
2026-02-04 13:15:32 +0530

    Updated Week3 Api and Server

--- Week3/Backend_Mern/server.js
+++ Week3/Backend_Mern/server.js
@@ -3,11 +3,11 @@ import {UserApp} from './Apis/UserApi.js'
 import {productapp} from './Apis/ProductApi.js'
 const app = express();
 
-app.listen(3001, () => {
+app.listen(3001, () => { // creates a server at 3001
   console.log("HTTP server listening on port 3001");
 });
 //body parsing middleware
 app.use(express.json())
 // creating middleware 
-app.use('/user-api',UserApp)
-app.use('/product-api',productapp)
+app.use('/user-api',UserApp) // only uses users routes 
+app.use('/product-api',productapp) // only uses product routes 
